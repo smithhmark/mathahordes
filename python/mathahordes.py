37324244ed1247b61a52c17f795b9ea9328cd6f2
Mark Smith <smith.h.mark@gmail.com>
2019-01-21 18:29:16 -0800

    wandering in the woods...

--- python/mathahordes.py
+++ python/mathahordes.py
@@ -1,7 +1,22 @@
 
+class Unit:
+    def __init__(self, name, stats, loadout, starAtts, starActs, type, size, solo):
+        self._name = name
+        self._solo = solo
+        self._stats = stats
+        self._loadout = loadout
+        self._starAtts = starAtts
+        self._starActs = starActs
+        self._type = type
+        self._size = size
+        self._models = []
+
 class Model:
     """What we need to track models"""
     def __init__(self, base, location, heading, mid, player):
+        self._inplay = False
+        self._onboard = False
+        self._ontable = False
         self._base = base
         self._loc = location
         self._heading = heading
@@ -22,3 +37,13 @@ class Game:
     def __init__(self):
         self._players = []
         self._board = None
+
+class Army:
+    """
+    """
+    def __init__(self):
+        pass
+
+def take_turn(active_player, battlefield, players):
+    maintenance_phase(active_player)
+    control_phase(active_player
