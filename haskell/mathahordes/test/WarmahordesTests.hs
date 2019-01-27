6a1eae6cff9ac5088589da9c5f53755d01a421c1
Mark Smith <smith.h.mark@gmail.com>
2019-01-27 10:00:12 -0800

    Added simple hunit test

---
+++ haskell/mathahordes/test/WarmahordesTests.hs
@@ -0,0 +1,20 @@
+module Main where
+
+import Test.HUnit
+
+import Warmahordes as WH
+
+testGetDiameter :: Test
+testGetDiameter = TestCase $ do
+        assertEqual "Small Bases are 30mm" 30 (WH.getDiameter SmallBase)
+        assertEqual "Medium Bases are 40mm" 40 (WH.getDiameter MediumBase)
+        assertEqual "Large Bases are 50mm" 50 (WH.getDiameter LargeBase)
+        assertEqual "Huge Bases are 120mm" 120 (WH.getDiameter HugeBase)
+
+testList = TestList [ TestLabel "testGetDiameter" testGetDiameter
+        ]
+
+main :: IO ()
+main = do
+  runTestTT testList
+  return ()
