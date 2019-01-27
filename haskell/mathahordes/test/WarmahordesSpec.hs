64585d7d412c124ba81fc2ae2793e184cefa4cc3
Mark Smith <smith.h.mark@gmail.com>
2019-01-27 12:29:58 -0800

    adding hspec tests

---
+++ haskell/mathahordes/test/WarmahordesSpec.hs
@@ -0,0 +1,16 @@
+module WarmahordesSpec (spec) where
+
+import Test.Hspec
+
+import Warmahordes
+
+spec = do 
+  describe "getDiameter" $ do
+    it "returns 30 for small bases" $
+      getDiameter SmallBase `shouldBe` 30
+    it "returns 40 for medium bases" $
+      getDiameter MediumBase `shouldBe` 40
+    it "returns 50 for large  bases" $
+      getDiameter LargeBase `shouldBe` 50
+    it "return 120 for huge bases" $
+      getDiameter HugeBase `shouldBe` 120
